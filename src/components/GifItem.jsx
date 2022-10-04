f09b7c2afb2e0e085fdb31b439024f132597f4f6
luisherrera98 <luchoherrerafernandez@gmail.com>
2022-10-04 15:34:16 -0300

    custom hook for fetch and loading

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const GifItem = ({id, title, url}) => {
+export const GifItem = ({id, title, url}) => {
   return (
     <div className='card'>
         <img src={url} alt={title} />
@@ -8,5 +8,3 @@ const GifItem = ({id, title, url}) => {
     </div>
   )
 }
-
-export default GifItem
